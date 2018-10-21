ec15a569945852bac70a6afb3e1bcc38c359eb80
tectiv3 <git@getpanda.me>
2018-10-22 00:20:23 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tectiv3/wiiscale/wiiboard"
+)
+
+func main() {
+	board, err := wiiboard.Detect()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	battery, err := board.Battery()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(battery)
+	go board.Listen()
+	// board.Calibrate()
+	for event := range board.Events {
+		fmt.Printf("%+v. Total: %0.2f, calibrated: %0.2f\n", event, event.Total/100, board.GetCalibrated()/100)
+	}
+}
