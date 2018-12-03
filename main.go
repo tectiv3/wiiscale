a61559af4e56ea532a6b486162111d076e146ab5
tectiv3 <git@getpanda.me>
2018-12-04 01:23:08 +0900

    Add mqtt

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/tectiv3/wiiscale/wiiboard"
 )
+
+var brokerURL = "1"
+
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	log.Printf("TOPIC: %s\n", msg.Topic())
+	log.Printf("MSG: %s\n", msg.Payload())
+}
 
 func main() {
 	board := wiiboard.New()
@@ -22,7 +32,37 @@ func main() {
 
 	go board.Listen()
 
+	// mqtt.DEBUG = log.New(os.Stdout, "", 0)
+	// mqtt.ERROR = log.New(os.Stdout, "", 0)
+	opts := mqtt.NewClientOptions().AddBroker("tcp://192.168.100.111:1883").SetClientID("wiiboard")
+	opts.SetKeepAlive(2 * time.Second)
+	opts.SetDefaultPublishHandler(f)
+	opts.SetPingTimeout(1 * time.Second)
+
+	c := mqtt.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	log.Println("Connected to mqtt")
+
 	for weight := range board.Weights {
-		log.Printf("Got weight: %0.2f\n", weight/100)
+		w := weight / 100
+		name := "kin"
+		if w < 60.0 {
+			name = "santy"
+		} else if w > 81.0 {
+			name = "domi"
+		}
+		log.Printf("%s: %0.2f\n", name, w)
+
+		// msg, _ := json.Marshal(struct {
+		//     Weight float64 `json:"weight"`
+		//     Person string  `json:"name"`
+		// }{w, name})
+		// token := c.Publish("sensors/wiiboard/last", 0, false, msg)
+
+		token := c.Publish("sensors/wiiboard/last", 0, false, fmt.Sprintf(`{"%s": %0.2f}`, name, w))
+		token.Wait()
 	}
 }
